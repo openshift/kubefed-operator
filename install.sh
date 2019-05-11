11b98054c628706c2a1f6c9173373c0b32a4cfd8
Sohan Kunkerkar <sohank2602@gmail.com>
2019-05-11 10:51:13 -0400

    Adding a script for automating operator installation instructions,

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# This script will create a namespace and deploy all the crds within the same
+# namespace
+# usage ./install.sh <Namespace>
+
+set -e
+
+NAMESPACE=$1
+
+if [ -z "$1" ]; then
+    echo "Enter the namespace after ./install.sh"
+    exit 1
+fi
+
+# create a namespace 
+kubectl create ns ${NAMESPACE}
+
+# Install crds 
+for f in ./deploy/crds/*.yaml ; do     
+  
+  kubectl apply -f "${f}" ; 
+
+done
+
+for f in ./deploy/resources/*.yaml ; do     
+
+  kubectl apply -f "${f}" --validate=false; 
+  
+done
+
+# Check if operator-sdk is installed or not and accordinlgy execute the commad.
+operator-sdk &> /dev/null
+if [ $? == 0 ]; then
+   operator-sdk up local  --namespace=${NAMESPACE}
+else
+   echo "Operator SDK is not installed."
+   exit 1
+fi
