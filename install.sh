fb961c2bb0bc03f778e71e26bd7f79c106b09bb7
Aniket Bhat <anbhat@redhat.com>
2019-05-16 16:33:33 -0400

    For non-local deployments, you need to install the resources/crds yaml yourself

--- install.sh
+++ install.sh
@@ -40,6 +40,9 @@ else
     for f in ./deploy/*.yaml ; do     
 	kubectl apply -f "${f}" --validate=false --namespace=${NAMESPACE} 
     done
+    for f in ./deploy/resources/*_crd.yaml ; do     
+	kubectl apply -f "${f}" --validate=false 
+    done
     echo "Deployed all the operator yamls for kubefed-operator in the cluster"
 fi
 
