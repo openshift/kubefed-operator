4732da5e9e3d9b6b4381d9b3376578760315dfb0
Sohan Kunkerkar <sohank2602@gmail.com>
2019-07-10 11:55:29 -0400

    Add a script to perform olm-e2e testing

---
+++ scripts/olm-e2e.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+./scripts/download-binaries.sh
+
+mv kubectl kubefedctl /go/bin/
+
+# Check for Namespaced-scoped deployment
+
+./scripts/smoke-test.sh -n kubefed-test -d olm-openshift -s Namespaced
+
+# Check for Cluster-scoped deployment
+
+./scripts/smoke-test.sh -n kubefed-test -d olm-openshift -s Cluster
