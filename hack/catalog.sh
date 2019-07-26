76e50c7e55768bbd14fc1b1fc04624dfba338c3a
Sohan Kunkerkar <sohank2602@gmail.com>
2019-07-26 15:45:58 -0400

    Small modifications to look for only one version of the  operator

--- hack/catalog.sh
+++ hack/catalog.sh
@@ -3,6 +3,8 @@
 CSV_FILE=$1
 DIR=${DIR:-$(cd $(dirname "$0")/.. && pwd)}
 NAME=${NAME:-$(ls $DIR/deploy/olm-catalog)}
+# Use the latest version of operator for testing
+OPERATOR_VERSION=${OPERATOR_VERSION:-0.1.0}
 
 x=( $(echo $NAME | tr '-' ' ') )
 DISPLAYNAME=${DISPLAYNAME:=${x[*]^}}
@@ -12,7 +14,7 @@ indent() {
   sed "s/^/$INDENT/" | sed "s/^${INDENT}\($1\)/${INDENT:0:-2}- \1/"
 }
 
-CRD=$(cat $(find $DIR/deploy/olm-catalog -name '*_crd.yaml' | sort -n) | grep -v -- "---" | indent apiVersion)
+CRD=$(cat $(find $DIR/deploy/olm-catalog/$NAME/$OPERATOR_VERSION -name '*_crd.yaml' | sort -n) | grep -v -- "---" | indent apiVersion)
 CSV=$(cat $CSV_FILE | indent apiVersion)
 PKG=$(cat $DIR/deploy/olm-catalog/$NAME/*package.yaml | indent packageName)
 
