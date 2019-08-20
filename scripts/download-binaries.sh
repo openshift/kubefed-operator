553ad6e30816528de9dc4727325dfb2bf2cb5af9
Sohan Kunkerkar <sohank2602@gmail.com>
2019-08-20 14:28:05 -0400

    search for bash in $PATH and then start the script

--- scripts/download-binaries.sh
+++ scripts/download-binaries.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script automates the downloading of binaries 
 # used for smoke testing
 CURL_ARGS="-Ls"
--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 NAMESPACE="${NAMESPACE:-default}"
 CLUSTERNAME="${CLUSTERNAME:-cluster1}"
